d08883fb9c75f1c0a4cc0b0135bee5d5654568ca
abauruel <abauruel@gmail.com>
2020-03-21 22:31:01 +0000

    change to stateless component

--- src/components/Cities/index.js
+++ src/components/Cities/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
+
 import { Container } from './styles';
 
-export default function Cities() {
+export default function Cities({ city }) {
   return (
     <Container>
       <ul>
         <li>
           <div>
+            <small>Day</small>
             <small>Min</small>
             <small>Max</small>
+            <small>City</small>
             <p />
           </div>
-          <div>
-            <p>20</p>
-            <p>28</p>
-            <p>Rio de Janeiro</p>
-          </div>
+          {city && (
+            <div>
+              <p>{city.forecasts[0]?.day}</p>
+              <p>{city.forecasts[0]?.low}&#186; C</p>
+              <p>{city.forecasts[0]?.high}&#186; C</p>
+              <p>{city.location?.city}</p>
+            </div>
+          )}
         </li>
       </ul>
     </Container>
