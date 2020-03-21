ed9c56c217e4803d48e4815a0c70127174528af3
abauruel <abauruel@gmail.com>
2020-03-21 22:31:21 +0000

    added column on Grid cities

--- src/components/Cities/styles.js
+++ src/components/Cities/styles.js
@@ -5,7 +5,7 @@ export const Container = styled.div`
   justify-content: center;
   div {
     display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-gap: 10px;
 
     & + div {
