745d39f69d6a63b38724c52c780a2cf9a85d9b2b
abauruel <abauruel@gmail.com>
2020-03-21 22:32:16 +0000

    changed background

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   form {
-    background: linear-gradient(-0deg, #fdc830, #f37335);
+    background: linear-gradient(-0deg, #00b4db, #0083b0);
     width: 600px;
     padding: 20px;
     display: flex;
@@ -16,14 +16,28 @@ export const Container = styled.div`
       font-weight: bold;
       color: #fff;
     }
-    input {
-      margin: 30px 20px;
-      width: 300px;
-      padding: 5px 10px;
-      font-size: 18px;
-      border: none;
-      border-radius: 4px;
+    > div {
+      display: flex;
+      flex-direction: row;
+      justify-content: center;
+      align-items: center;
+      button {
+        margin-top: 1px;
+        margin-left: -50px;
+        padding: 3px;
+        border: none;
+        cursor: pointer;
+      }
+      input {
+        margin: 30px 20px;
+        width: 300px;
+        padding: 5px 30px 5px 10px;
+        font-size: 18px;
+        border: none;
+        border-radius: 4px;
+      }
     }
+
     hr {
       margin: 10px;
       border: 0.5px solid #fff;
